eb42c355c5118d7f2b0dfe1c77d00c75423e0940
hoangviet15ht <hoangviet15ht@gmail.com>
2021-12-09 19:58:53 +0700

    add lab 09

---
+++ Programming/PlaceRushOrderController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import java.util.regex.Pattern;
+
+/**
+ * class PlaceRushOrderController.java control the flow of place rush order usecase in our AIMS project
+ *
+ * @author HoangViet
+ */
+public class PlaceRushOrderController {
+
+    /**
+     * This method check delivery instruction can be verified or not
+     * @param instruction
+     * @return true if the input is in the correct format
+     */
+    public boolean validateDeliveryInstruction(String instruction){
+        if(instruction==null)return false;
+        return true;
+    }
+
+    /**
+     * This method checks if the input date is in true or false format
+     * @param date
+     * @return true if the input is in the correct format
+     */
+    public boolean validateDeliveryDate(String date){
+        if(date == null) return false;
+        return Pattern.matches("^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$",date);
+    }
+
+    /**
+     * This method checks if the input time is in true or false format
+     * @param time
+     * @return true if the input is in the correct format
+     */
+    public boolean validateDeliveryTime(String time){
+        if(time == null) return false;
+        return Pattern.matches("\t^([0-1][0-9]|[2][0-3]):([0-5][0-9])$",time);
+    }
+}
